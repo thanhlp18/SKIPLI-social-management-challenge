9dd13cc07a42fa75b69442af769abaa32a691fa2
Phuoc Thanh <thanhlp18@gmail.com>
2023-08-30 14:47:55 +0700

    (get) generateAccessCode and (post) validateAccessCode)

--- server/app.js
+++ server/app.js
@@ -1,15 +1,40 @@
+// --------------
+// Start express config
 var express = require("express");
 const cors = require("cors");
 var app = express();
 require("dotenv").config();
-console.log(
-  "Your environment variable TWILIO_ACCOUNT_SID has the value: ",
-  process.env.TWILIO_ACCOUNT_SID
-);
+var multer = require("multer");
+var upload = multer();
+// End express config
+
+// Start firebase config
+const { initializeApp } = require("firebase/app");
+const {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  setDoc,
+  getDoc,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} = require("firebase/firestore/lite");
+const firebaseConfig = require("./.firebaseConfig/firebaseConfig.json");
+const firebaseStore = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseStore);
+// End firebase config
 
 // Enable CORS for all routes
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.array());
+app.use(express.static("public"));
 
+// -------------- method
+//Start validate phone number when use input
 app.get("/validate-phone-number", (req, res) => {
   const phoneNumber = req.query.phoneNumber;
   const countryCode = req.query.countryCode;
@@ -34,6 +59,60 @@ app.get("/validate-phone-number", (req, res) => {
       console.error("Error:", error);
       res.status(500).json({ error: "An error occurred" }); // Handle errors and send an error response
     });
+});
+//End validate phone number when use input
+
+//START OTP HANDLE
+// Generate a random 6-digit access code
+function generateAccessCode() {
+  return Math.floor(100000 + Math.random() * 900000).toString();
+}
+
+// Save the access code to the phone number in Firebase Realtime Database
+app.post("/generate-access-code", async (req, res) => {
+  const phoneNumber = req.body.phoneNumber;
+  const timeStamp = serverTimestamp();
+  const accessCode = generateAccessCode();
+
+  // addDoc(collection(db, "users")
+  try {
+    const userRef = await setDoc(doc(db, "users", phoneNumber), {
+      accessCode: accessCode,
+      createdDate: timeStamp,
+    }).then(() => {
+      res.json(accessCode); // Send the data back to the client
+    });
+    // console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+});
+
+// Validate access code
+// Save the access code to the phone number in Firebase Realtime Database
+app.post("/validate-access-code", async (req, res) => {
+  const phoneNumber = req.body.phoneNumber;
+  const accessCode = req.body.accessCode;
+
+  try {
+    // Look up and get the doc to the phone number of user
+    const usersRef = doc(db, "users", phoneNumber);
+    const docSnap = await getDoc(usersRef);
+
+    // Compare the access code store in data base, return true and delete the access code if they input a valid OTP
+    // else return false
+    if (docSnap.data().accessCode === accessCode) {
+      res.json(true);
+      // set access code to empty string
+      await updateDoc(usersRef, {
+        accessCode: "",
+      });
+    } else {
+      res.json(false);
+    }
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 });
 
 app.listen(3001, () => {
---
+++ server/firebase.js
@@ -0,0 +1,21 @@
+const { initializeApp } = require("firebase/app");
+const {
+  getFirestore,
+  collection,
+  getDocs,
+} = require("firebase/firestore/lite");
+const firebaseConfig = require("./.firebaseConfig/firebaseConfig.json");
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, "user");
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map((doc) => doc.data());
+  return cityList;
+}
+getCities(db).then((res) => {
+  console.log(res);
+});
